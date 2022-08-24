79f298e69abc563da9d7e239e58947890579785d
myumx <7685659+myumx@users.noreply.github.com>
2022-08-25 00:23:18 +0900

    Create API client

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,24 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
+
+import { execTest } from "lib/api/test"
 
 const App: React.FC = () => {
+  const [message, setMessage] = useState<string>("")
+
+  const handleExecTest = async () => {
+    const res = await execTest()
+
+    if (res.status === 200) {
+      setMessage(res.data.message)
+    }
+  }
+
+  useEffect(() => {
+    handleExecTest()
+  }, [])
+
   return (
-    <h1>Hello World!</h1>
+    <h1>{message}</h1>
   )
 }
 
