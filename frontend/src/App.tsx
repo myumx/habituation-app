ccb66059252b411dc75e11bfb658c51ffc385117
myumx <7685659+myumx@users.noreply.github.com>
2022-09-04 13:42:52 +0900

    fix frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,41 @@
+//import * as React from "react"
 import React, { useEffect, useState } from "react"
-
 import { execTest } from "lib/api/test"
-
-const App: React.FC = () => {
-  const [message, setMessage] = useState<string>("")
-
-  const handleExecTest = async () => {
-    const res = await execTest()
-
-    if (res.status === 200) {
-      setMessage(res.data.message)
-    }
-  }
-
-  useEffect(() => {
-    handleExecTest()
-  }, [])
-
-  return (
-    <h1>{message}</h1>
-  )
+import {
+  ChakraProvider,
+  Box,
+  Text,
+  Link,
+  VStack,
+  Code,
+  Grid,
+  theme,
 }
+from "@chakra-ui/react"
+import { ColorModeSwitcher } from "./ColorModeSwitcher"
+import { Logo } from "./Logo"
 
-export default App
+export const App = () => (
+  <ChakraProvider theme={theme}>
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        <VStack spacing={8}>
+          <Logo h="40vmin" pointerEvents="none" />
+          <Text>
+            Edit <Code fontSize="xl">src/App.tsx</Code> and save to reload.
+          </Text>
+          <Link
+            color="teal.500"
+            href="https://chakra-ui.com"
+            fontSize="2xl"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn Chakra
+          </Link>
+        </VStack>
+      </Grid>
+    </Box>
+  </ChakraProvider>
+)
